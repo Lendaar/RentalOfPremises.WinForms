5473073c95f2a1de3faecbbac82a33e0a729b348
Lendaar <voronenkov04@mail.ru>
2024-05-20 21:26:49 +0300

    Добавил конвертер enum - строка для TypeRoles

---
+++ RentalOfPremises.WinForms/Enums/EnumRoleTypesConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace RentalOfPremises.WinForms.Enums
+{
+    public class EnumRoleTypesConverter
+    {
+        public RoleTypes RoleType { get; set; }
+
+        public string DislplayValue { get; set; }
+
+        public EnumRoleTypesConverter(RoleTypes roleType)
+        {
+            this.RoleType = roleType;
+            DislplayValue = roleType.PerevodDescription() ?? string.Empty;
+        }
+    }
+}
