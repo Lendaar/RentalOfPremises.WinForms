bdd2d9da0d805390583c305eb39cccfaaa7dbf51
Lendaar <voronenkov04@mail.ru>
2024-05-20 21:15:04 +0300

    HttpClient для работы с котроллерами User

---
+++ RentalOfPremises.WinForms/BL/UserHttpClient.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json;
+using RentalOfPremises.WinForms.Models;
+using RentalOfPremises.WinForms.ModelsRequest.User;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentalOfPremises.WinForms.BL
+{
+    public class UserHttpClient
+    {
+        public static async Task<List<UserResponse>> GetUsers()
+        {
+            try
+            {
+                var client = new GetHttpClient().GetClient();
+                var users = await client.GetAsync("User/");
+                users.EnsureSuccessStatusCode();
+                var s = users.StatusCode;
+                var rez = await users.Content.ReadAsStringAsync();
+
+                if (users.StatusCode == HttpStatusCode.OK)
+                {
+                    var usersResponse = (JsonConvert.DeserializeObject<List<UserResponse>>(rez));
+                    usersResponse.ForEach(x => x.InitFio());
+
+                    return usersResponse;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Не удалось установить соединение с сервером", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public static async Task<DialogResult> CreateUser(UserRequest userRequest)
+        {
+            try
+            {
+                var client = new GetHttpClient().GetClient();
+                var json = JsonConvert.SerializeObject(userRequest);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var users = await client.PostAsync("User/", data);
+                users.EnsureSuccessStatusCode();
+                MessageBox.Show("Пользователь создан", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return DialogResult.OK;
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось установить соединение с сервером", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return DialogResult.Cancel;
+            }
+        }
+
+        public static async Task DeleteUser(Guid id)
+        {
+            try
+            {
+                var client = new GetHttpClient().GetClient();
+                var users = await client.DeleteAsync("User/" + id);
+                users.EnsureSuccessStatusCode();
+                MessageBox.Show("Пользователь удален", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось установить соединение с сервером", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static async Task<DialogResult> UpdateUser(UserRequest userRequest)
+        {
+            try
+            {
+                var client = new GetHttpClient().GetClient();
+                var json = JsonConvert.SerializeObject(userRequest);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+                var users = await client.PutAsync("User/", data);
+                if (users.StatusCode.Equals(HttpStatusCode.Conflict))
+                {
+                    MessageBox.Show("Ошибка валидации", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return DialogResult.No;
+                }
+                
+                users.EnsureSuccessStatusCode();
+
+                MessageBox.Show("Запись обновлена", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return DialogResult.OK;
+            }
+            catch
+            {
+                MessageBox.Show("Не удалось установить соединение с сервером", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return DialogResult.No;
+            }
+        }
+    }
+}
