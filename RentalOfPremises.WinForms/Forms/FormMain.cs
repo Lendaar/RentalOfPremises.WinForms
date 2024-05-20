38143a80f7d43ab0e24546ed6e3f4a5b86d9f291
Lendaar <voronenkov04@mail.ru>
2024-05-20 21:31:18 +0300

    Обновление данных при переходе по вкладкам меню

--- RentalOfPremises.WinForms/Forms/FormMain.cs
+++ RentalOfPremises.WinForms/Forms/FormMain.cs
@@ -14,6 +14,11 @@ namespace RentalOfPremises.WinForms.Forms
         private void materialTabControl1_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             this.Text = materialTabControl1.SelectedTab.Tag.ToString();
+            switch (materialTabControl1.SelectedIndex)
+            {
+                case 5:
+                    userControlUser3.UserControlUser_Load(sender, e); break;
+            }
         }
 
         private void Form1_Load(object sender, System.EventArgs e)
