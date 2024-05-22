29eb691b29737d36040f364aa7a32aa065ba6ffe
Lendaar <voronenkov04@mail.ru>
2024-05-22 18:26:49 +0300

    Сделал Enum-конвертер универсальным

---
+++ RentalOfPremises.WinForms/Enums/EnumConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RentalOfPremises.WinForms.Enums
+{
+    public class EnumConverter
+    {
+        public Enum Type { get; set; }
+
+        public string DislplayValue { get; set; }
+
+        public EnumConverter(Enum roleType)
+        {
+            this.Type = roleType;
+            DislplayValue = roleType.PerevodDescription() ?? string.Empty;
+        }
+    }
+}
